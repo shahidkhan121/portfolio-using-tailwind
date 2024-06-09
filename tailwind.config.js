66f83f82d6b7d6ff54ec7b263c1cdb0592cb6c1b
shahidkhan98 <155524277+shahidkhan121@users.noreply.github.com>
2024-06-09 12:51:51 +0500

    Add files via upload
    .

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      screens:{
+        sm:"480px",
+        md:"768px",
+        lg:"1024px",
+        xl:"1280px",
+      },
+spacing:{
+  "big":"48rem"
+}
+    },
+    fontFamily:{
+      nunito:[' Nunito', 'sans-serif']
+    }
+  },
+ 
+  
+  plugins: [],
+}
+
